50420195bbd4db797fa341d9a1966d1c0563105b
leonardopadilha <leonardopadilha28@gmail.com>
2020-05-24 12:06:22 -0300

    Alterando classe AumentoSalarial

--- EstudosPHP/Exercicios/AumentoSalario/Execucao/AumentoSalarial.php
+++ EstudosPHP/Exercicios/AumentoSalario/Execucao/AumentoSalarial.php
@@ -1,36 +1,36 @@
-<?php
-
-
+class AumentoSalarial
+{
 $salarioFinal = 0;
 
-function calculaAumentoSalarial($salario)
-{
-    if ($salario >= 0 && $salario <= 400.00){
-        echo "Novo Salário: {$salario + ($salarioFinal = $salario * 0.15)} \n
+    function calculaAumentoSalarial($salario)
+    {
+        if ($salario >= 0 && $salario <= 400.00){
+            print "Novo Salário: {$salario + ($salarioFinal = $salario * 0.15)} \n
               Reajuste ganho: {($salarioFinal = $salario * 0.15)} \n
               Em percentual: 15%";
 
-    }elseif ($salario >= 400.01 && $salario <= 800.00){
-        echo "Novo Salário: {$salario + ($salarioFinal = $salario * 0.12)} \n
+        }elseif ($salario >= 400.01 && $salario <= 800.00){
+            print "Novo Salário: {$salario + ($salarioFinal = $salario * 0.12)} \n
               Reajuste ganho: {($salarioFinal = $salario * 0.12)} \n
               Em percentual: 12%";
 
-    }elseif ($salario >= 800.01 && $salario <= 1200.00) {
-        echo "Novo Salário: {$salario + ($salarioFinal = $salario * 0.1)} \n
+        }elseif ($salario >= 800.01 && $salario <= 1200.00) {
+            print "Novo Salário: {$salario + ($salarioFinal = $salario * 0.1)} \n
               Reajuste ganho: {($salarioFinal = $salario * 0.1)} \n
               Em percentual: 10%";
 
-    }elseif ($salario >= 1200.01 && $salario <= 2000.00) {
-        echo "Novo Salário: {$salario + ($salarioFinal = $salario * 0.07)} \n
+        }elseif ($salario >= 1200.01 && $salario <= 2000.00) {
+            print "Novo Salário: {$salario + ($salarioFinal = $salario * 0.07)} \n
               Reajuste ganho: {($salarioFinal = $salario * 0.07)} \n
               Em percentual: 7%";
 
-    }elseif ($salario >= 2000.01) {
-        echo "Novo Salário: {$salario + ($salarioFinal = $salario * 0.04)} \n
+        }elseif ($salario >= 2000.01) {
+            print "Novo Salário: {$salario + ($salarioFinal = $salario * 0.04)} \n
               Reajuste ganho: {($salarioFinal = $salario * 0.04)} \n
               Em percentual: 4%";
-    }else{
-        echo "Digite um valor válido para um salário";
+        }else{
+            echo "Digite um valor válido para um salário";
+        }
     }
-}
 
+}
